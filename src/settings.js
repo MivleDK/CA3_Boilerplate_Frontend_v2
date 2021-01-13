4e2f9dcf4df6af8751e75872fadf05e591b7b25c
Amazingh0rse <therahbek@gmail.com>
2021-01-13 12:41:28 +0100

    changes to links in settings

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
-export const URL = "http://localhost:8080/CA3_Boilerplate";
-export const URLJokes = "http://localhost:8080/CA3_Boilerplate/api/jokes";
-export const URLScrapeSequential = "http://localhost:8080/CA3_Boilerplate/api/scrape/sequential";
-export const URLScrapeParallel = "http://localhost:8080/CA3_Boilerplate/api/scrape/parallel";
+export const URL = "http://localhost:8080/3_sem_eksamensprojekt";
+export const URLJokes = "http://localhost:8080/3_sem_eksamensprojekt/api/jokes";
+export const URLScrapeSequential = "http://localhost:8080/3_sem_eksamensprojekt/api/scrape/sequential";
+export const URLScrapeParallel = "http://localhost:8080/3_sem_eksamensprojekt/api/scrape/parallel";
